90b8eafb9e72cb6fc3e44110be06c5d8a5cc1462
just-hms <alessandroversari1@gmail.com>
2023-05-28 16:58:51 +0200

    add: example

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
-# $1 outputfolder
-# $2 dataset
-# $3 K
-
+# creating output folder
 mkdir -p $1
+
+# storing used conf
+mkdir -p $1
+echo "Dataset: $2" >> $1/.conf
+echo "K: $3" >> $1/.conf
+
+# running python
 python3 compare/kmeans.py $2 $1 $3  
+
+# running hadoop
 ./hadoop.sh $1/start.csv $2 $1
