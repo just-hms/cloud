36a3635cde463fb4e8c5acd2f7c94ddee57d19e8
just-hms <alessandroversari1@gmail.com>
2023-05-30 22:55:32 +0200

    add: python.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 # creating output folder
+# $1 output folder
+# $2 dataset
+# $3 K used for kmeans
+
 mkdir -p $1
 
 # storing used conf
-mkdir -p $1
 echo "Dataset: $2" >> $1/.conf
 echo "K: $3" >> $1/.conf
 
 # running python
-python3 compare/kmeans.py $2 $1 $3  
+./python.sh $1 $2 $3  
 
 # running hadoop
 ./hadoop.sh $1/start.csv $2 $1
