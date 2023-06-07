40e3759f2914a811abf7763d8a34535d5b7fe656
just-hms <alessandroversari1@gmail.com>
2023-06-07 15:36:54 +0200

    removed: todos

--- hadoop.sh
+++ hadoop.sh
@@ -1,4 +1,3 @@
-# TODO get this as input
 JAR="cloud-1.0-SNAPSHOT.jar"
 SSH_KEY="~/.ssh/keys/cloud/key"
 SCRIPT="\033[1;30m[HADOOP.SH]\033[0m"
@@ -7,15 +6,12 @@ centroidsFilename="${1##*/}"
 dataset="${2##*/}"
 OUTPUT=$3
 
-# TODO had check for edits
 echo "$SCRIPT Building..."
 mvn clean package
 if [ $? -ne 0 ]; then
     echo failed to build
     exit
 fi
-
-# TODO check if the vpn is working
 
 echo "$SCRIPT Copying .jar and specified files to the remote server"
 rsync -u -e "ssh -i $SSH_KEY" target/$JAR $1 hadoop@cloud-hms:repos
